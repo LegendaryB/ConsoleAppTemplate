912433f60fb13e9cd7867735812b26474cafae64
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2024-03-03 17:16:55 +0100

    Added ILogger<App>

--- src/ConsoleAppTemplate/App.cs
+++ src/ConsoleAppTemplate/App.cs
@@ -1,7 +1,16 @@
-﻿namespace ConsoleAppTemplate
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppTemplate
 {
     internal class App
     {
+        private readonly ILogger<App> _logger;
+
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task RunAsync(CancellationToken cancellationToken = default)
         {
 
